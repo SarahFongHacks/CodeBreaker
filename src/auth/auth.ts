7590e13abbc2e1bdcc47bae7c14be634b7b35d35
JugadK <curaww@gmail.com>
2022-09-15 23:59:19 -0700

    fix some formatting

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -5,22 +5,21 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 
-
 export async function register(auth: Auth, email: string, password: string) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-      console.log(user)
+      console.log(user);
 
-	  // ...
+      // ...
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      
-	  console.log(errorCode + " " + errorMessage);
-	  // ..
+
+      console.log(errorCode + " " + errorMessage);
+      // ..
     });
 }
 
@@ -36,8 +35,7 @@ export async function signIn(auth: Auth, email: string, password: string) {
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-    
-	  console.log(errorCode + " " + errorMessage);
-	});
-}
 
+      console.log(errorCode + " " + errorMessage);
+    });
+}
