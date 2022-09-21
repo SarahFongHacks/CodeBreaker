0d70eb5e2864345cd07c0c184655687f755bf79e
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-09-20 22:25:18 -0700

    create login

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Register = () => {
+const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -12,23 +12,23 @@ const Register = () => {
           id="username"
           type="text"
           autoComplete="off"
-          className=" rounded-md px-2 py-2 placeholder-gray-400 focus:outline-none border-[1px] border-black"
-          placeholder="Email"
+          className=" rounded-md px-2 py-2 placeholder-gray-400 focus:outline-none border-2 border-black"
+          placeholder="Username"
           onChange={(e) => setUsername(e.target.value)}
         ></input>
         <input
           id="password"
           type="password"
-          className=" rounded-md px-2 py-2 placeholder-gray-400 focus:outline-none border-[1px] border-black"
+          className=" rounded-md px-2 py-2 placeholder-gray-400 focus:outline-none border-2 border-black"
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
         ></input>
-        <div className="cursor-pointer rounded-md w-full bg-black text-white p-2 flex items-center justify-center">
-          Register
-        </div>
       </form>
+      <div className="cursor-pointer rounded-md w-full bg-black text-white p-2 flex items-center justify-center">
+        Login
+      </div>
     </div>
   );
 };
 
-export default Register;
+export default Login;
