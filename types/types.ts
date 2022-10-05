542ec9ff5aabe323921f7c83fd14aadfbe49c0b2
JugadK <curaww@gmail.com>
2022-10-05 12:02:10 -0700

    Fix formatting

--- types/types.ts
+++ types/types.ts
@@ -5,41 +5,37 @@
 // Hotel Room
 
 export type HotelRoom = {
-	id : number
-	roomNumber : number
-	price : number 
-	numberOfBeds : number  
-	numberOfBathrooms : number
-	reservations : Reservation[]
-	capacity : number
-	hotel : string
- imageURL : string
-}
+  id: number;
+  roomNumber: number;
+  price: number;
+  numberOfBeds: number;
+  numberOfBathrooms: number;
+  reservations: Reservation[];
+  capacity: number;
+  hotel: string;
+  imageURL: string;
+};
 
 export type Reservation = {
-	id : number
-	user : User
-	startDate : Date
-	endDate : Date
-	hotelRoom : HotelRoom
-}
+  id: number;
+  user: User;
+  startDate: Date;
+  endDate: Date;
+  hotelRoom: HotelRoom;
+};
 
 export type User = {
-
-	name : string
-	currentBooking : Reservation[] 	
-}
+  name: string;
+  currentBooking: Reservation[];
+};
 
 export type FireBaseError = {
-  
-  error : boolean 
-  errorCode : string 
-  errorMessage : string
-}
+  error: boolean;
+  errorCode: string;
+  errorMessage: string;
+};
 
 export type UserLoginCred = {
-  
-  userCred : any
-  error : FireBaseError
-
-} 
+  userCred: any;
+  error: FireBaseError;
+};
