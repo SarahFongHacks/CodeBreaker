9bdc1e0a918f84117017e6ac0ab64696c7cc2224
JugadK <curaww@gmail.com>
2022-10-07 19:02:25 -0700

    Add changeReservation function

--- db_func/reservations.ts
+++ db_func/reservations.ts
@@ -1,13 +1,19 @@
-import {dbConverter} from "../db_conversion/db_converter"
-import {Reservation} from "../types/types"
-import {db} from "../pages/index"
+import { dbConverter } from "../db_conversion/db_converter";
+import { Reservation } from "../types/types";
+import { db } from "../pages/index";
+import { setDoc } from "firebase/firestore";
 
-export async function changeReservationDate(reservation : Reservation, startDate : Date, endDate : Date) {
+export async function changeReservationDate(
+  reservation: Reservation,
+  startDate: Date,
+  endDate: Date
+) {
+  reservation.startDate = startDate.getTime();
+  reservation.endDate = endDate.getTime();
 
+  await writeReservation(reservation);
 }
 
-async function writeReservationToFirebase(reservation : Reservation) {
-
-	
-
+async function writeReservation(reservation: Reservation) {
+  await setDoc(reservation.docRef, dbConverter.reservationToJson(reservation));
 }
