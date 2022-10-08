e4a8a47923762ac7b9711180a81fe5431ad4945b
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-10-08 00:46:04 -0700

    styling hotels

--- components/Hotels.tsx
+++ components/Hotels.tsx
@@ -6,14 +6,20 @@ const Hotels = () => {
   console.log(hotels);
 
   return (
-    <div className="w-full h-screen grid grid-cols-4">
+    <div className="w-full min-h-screen grid grid-cols-4 gap-4 p-8 ">
       {hotels.map((hotel) => {
         return (
-          <div className="flex flex-col items-center justify-center ">
-            {/* <div className="w-24 h-24 rounded-lg square bg-secondary" /> */}
-            <p>{hotel.hotel}</p>
-            <p>Price: ${hotel.price}</p>
-            <p>Location: {hotel.location}</p>
+          <div className="flex flex-col items-center justify-center text-sm p-4 cursor-pointer group ">
+            <div className="w-full rounded-lg aspect-square bg-red-500 overflow-hidden">
+              <img
+                src="./assets/test.webp"
+                className="w-full h-full object-cover group-hover:scale-110 transition duration-200 ease-linear"
+              />
+            </div>
+
+            <p className="font-bold mt-4">{hotel.hotel}</p>
+            <p className="text-gray-800 mt-2">Price: ${hotel.price}</p>
+            <p className="text-gray-800">Location: {hotel.location}</p>
           </div>
         );
       })}
