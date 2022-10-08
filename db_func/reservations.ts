50de9b664f681cb3b71171e4c8a61c1ca9306e44
JugadK <curaww@gmail.com>
2022-10-07 18:12:29 -0700

    Add location to hotel room

---
+++ db_func/reservations.ts
@@ -0,0 +1,13 @@
+import {dbConverter} from "../db_conversion/db_converter"
+import {Reservation} from "../types/types"
+import {db} from "../pages/index"
+
+export async function changeReservationDate(reservation : Reservation, startDate : Date, endDate : Date) {
+
+}
+
+async function writeReservationToFirebase(reservation : Reservation) {
+
+	
+
+}
