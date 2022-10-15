ac337d70592b79711dd000ecaf8e77073dbc982b
Jugad Khajuria <curaww@gmail.com>
2022-10-14 21:39:50 -0700

    formatting

--- db_func/reservations.ts
+++ db_func/reservations.ts
@@ -1,11 +1,14 @@
-import {collection, doc, setDoc} from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
 
-import {dbConverter} from "../db_conversion/db_converter";
-import {db} from "../pages/index";
-import {HotelRoom, Reservation, User} from "../types/types";
+import { dbConverter } from "../db_conversion/db_converter";
+import { db } from "../pages/index";
+import { HotelRoom, Reservation, User } from "../types/types";
 
-export async function changeReservationDate(reservation: Reservation,
-                                            startDate: Date, endDate: Date) {
+export async function changeReservationDate(
+  reservation: Reservation,
+  startDate: Date,
+  endDate: Date
+) {
   reservation.startDate = startDate.getTime();
   reservation.endDate = endDate.getTime();
 
@@ -16,21 +19,24 @@ async function writeReservation(reservation: Reservation) {
   await setDoc(reservation.docRef, dbConverter.reservationToJson(reservation));
 }
 
-export async function createReservation(hotelRoom: HotelRoom, user: User,
-                                        startDate: Date, endDate: Date) {
-
+export async function createReservation(
+  hotelRoom: HotelRoom,
+  user: User,
+  startDate: Date,
+  endDate: Date
+) {
   const collectionRef = collection(db, "User");
   const docRef = doc(collectionRef);
   const id = docRef.id;
 
   const reservation: Reservation = {
-    id : id,
-    docRef: docRef, 
-    endDate : endDate.getTime(),
+    id: id,
+    docRef: docRef,
+    endDate: endDate.getTime(),
     hotelRoomId: hotelRoom.id,
     startDate: startDate.getTime(),
-    userId: user.id
-  }
+    userId: user.id,
+  };
 
-  writeReservation(reservation)
+  writeReservation(reservation);
 }
