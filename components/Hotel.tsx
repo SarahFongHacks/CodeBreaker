a6e19b815fe6cf25a748b74d29b6e3a4219b85eb
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-10-14 19:39:32 -0700

    styling hotel icons

--- components/Hotel.tsx
+++ components/Hotel.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+import { MdLocationPin, MdPeopleAlt } from "react-icons/md";
+import { IoIosBed } from "react-icons/io";
+import { FaMoneyBillWave, FaToilet } from "react-icons/fa";
+import { BsFillDoorOpenFill } from "react-icons/bs";
 
 const Hotel = ({ hotels }) => {
   const router = useRouter();
@@ -18,30 +22,50 @@ const Hotel = ({ hotels }) => {
           />
         </div>
         <div className="w-1/2 h-full flex flex-row space-between items-start">
-          <div className="w-full h-full grid grid-cols-2 gap-2 text-lg items-start justify-between ">
-            <div className="flex flex-col">
-              <p className="font-semibold">Location</p>
-              <p className="text-base mt-1">{hotel.location}</p>
+          <div className="w-full h-full grid grid-cols-2 gap-4 text-lg items-start justify-between ">
+            <div className="flex flex-row w-full items-center justify-between">
+              <div className="flex flex-col">
+                <p className="font-semibold">Location</p>
+                <p className="text-base mt-1">{hotel.location}</p>
+              </div>
+              <MdLocationPin className="mx-4" />
             </div>
-            <div className="flex flex-col">
-              <p className="font-semibold">Capacity</p>
-              <p className="text-base mt-1">{hotel.capacity} Guests</p>
+            <div className="flex flex-row w-full items-center justify-between">
+              <div className="flex flex-col">
+                <p className="font-semibold">Capacity</p>
+                <p className="text-base mt-1">{hotel.capacity} Guests</p>
+              </div>
+              <MdPeopleAlt className="mx-4" />
             </div>
-            <div className="flex flex-col">
-              <p className="font-semibold">Bedrooms</p>
-              <p className="text-base mt-1">{hotel.numberOfBeds} beds</p>
+            <div className="flex flex-row w-full items-center justify-between">
+              <div className="flex flex-col">
+                <p className="font-semibold">Bedrooms</p>
+                <p className="text-base mt-1">{hotel.numberOfBeds} beds</p>
+              </div>
+              <IoIosBed className="mx-4" />
             </div>
-            <div className="flex flex-col">
-              <p className="font-semibold">Bathrooms</p>
-              <p className="text-base mt-1">{hotel.numberOfBathrooms} baths</p>
+            <div className="flex flex-row w-full items-center justify-between">
+              <div className="flex flex-col">
+                <p className="font-semibold">Bathrooms</p>
+                <p className="text-base mt-1">
+                  {hotel.numberOfBathrooms} baths
+                </p>
+              </div>
+              <FaToilet className="mx-4" />
             </div>
-            <div className="flex flex-col">
-              <p className="font-semibold">Room Number</p>
-              <p className="text-base mt-1">{hotel.roomNumber}</p>
+            <div className="flex flex-row w-full items-center justify-between">
+              <div className="flex flex-col">
+                <p className="font-semibold">Room Number</p>
+                <p className="text-base mt-1">{hotel.roomNumber}</p>
+              </div>
+              <BsFillDoorOpenFill className="mx-4" />
             </div>
-            <div className="flex flex-col">
-              <p className="font-semibold">Rate </p>
-              <p className="text-base mt-1">${hotel.price}/night</p>
+            <div className="flex flex-row w-full items-center justify-between">
+              <div className="flex flex-col">
+                <p className="font-semibold">Rate</p>
+                <p className="text-base mt-1">${hotel.price}/night</p>
+              </div>
+              <FaMoneyBillWave className="mx-4" />
             </div>
             <div className="w-full col-span-2 mt-16 place-self-end shadow-md cursor-pointer ring-tertiary text-tertiary  py-3 px-5 ring-1 transition ease-linear duration-200 rounded-md  whitespace-nowrap flex items-center justify-center hover:bg-tertiary hover:text-white hover:ring-tertiary">
               Book Now
