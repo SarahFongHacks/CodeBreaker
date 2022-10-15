d2350ba13cc2c3f6f5cfcc69840498e14c36f7f3
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-10-14 18:16:28 -0700

    wip individual hotel page

--- components/Hotel.tsx
+++ components/Hotel.tsx
@@ -1,29 +1,11 @@
-import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/router";
 
-const Hotel = ({ hotel }) => {
-  return (
-    <Link href={"/rooms/" + hotel.id}>
-      <div className="flex flex-col items-center justify-center text-sm cursor-pointer group ">
-        <div className="w-full rounded-lg aspect-square bg-red-500 overflow-hidden">
-          <img
-            src="./assets/test.webp"
-            className="w-full h-full object-cover group-hover:scale-110 transition duration-200 ease-linear"
-          />
-        </div>
-        <p className="font-bold mt-4">{hotel.hotel}</p>
-        <div className="w-full flex flex-row space-between items-start">
-          <div className="w-full flex flex-col">
-            <p className="text-gray-800 mt-2">Price: ${hotel.price}</p>
-            <p className="text-gray-800">Location: {hotel.location}</p>
-          </div>
-          <div className="shadow-md cursor-pointer ring-tertiary text-tertiary  py-3 px-5 ring-1 transition ease-linear duration-200 rounded-md  whitespace-nowrap flex items-center justify-center group-hover:bg-tertiary group-hover:text-white group-hover:ring-tertiary">
-            Book Now
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
+const Hotel = ({ hotels }) => {
+  const router = useRouter();
+  console.log(router.pathname);
+  const hotel = hotels.find((hotel) => hotel.id === "1");
+  return <div>{router.pathname}</div>;
 };
 
 export default Hotel;
