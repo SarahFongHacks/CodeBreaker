d6974cf3c83b7c7f6205702f2e53a8dc8ddef1dd
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-10-22 16:51:25 -0700

    add main page text animations

--- components/MainPage.js
+++ components/MainPage.js
@@ -50,16 +50,40 @@ const Main = () => {
       </div>
 
       <div className="w-full relative flex flex-col items-center justify-center mt-64">
-        <h1 className="text-8xl font-bold tracking-tighter text-white pb-2">
-          Booker's Paradise
-        </h1>
+        <div className="overflow-hidden">
+          <motion.h1
+            className="text-8xl font-bold tracking-tighter text-white pb-2"
+            initial={{ y: 50, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { delay: 0.4, duration: 0.5 },
+            }}
+          >
+            Booker's Paradise
+          </motion.h1>
+        </div>
         <p className="text-white tracking-widest uppercase text-xl">
           Find your escape
         </p>
       </div>
       <div className=" absolute bottom-16 w-full items-center flex justify-center  z-10">
         <Link href="/hotels">
-          <BsArrowDownCircle className="cursor-pointer text-5xl hover:scale-110 transition duration-200 ease-linear text-white/50 hover:text-white" />
+          <motion.div
+            className=""
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{
+              scale: 1,
+              opacity: 1,
+              transition: {
+                delay: 0.4,
+                duration: 0.5,
+                bounce: 0.5,
+              },
+            }}
+          >
+            <BsArrowDownCircle className="cursor-pointer text-5xl hover:scale-110 transition duration-200 ease-linear text-white/50 hover:text-white" />
+          </motion.div>
         </Link>
       </div>
       <div className="w-full h-16 bg-gradient-to-r from-white to-white absolute bottom-0 blur-[100px] opacity-30 z-50" />
