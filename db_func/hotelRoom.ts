72433cf638d609fb791ff2d70a20b32b707b9a0e
JugadK <curaww@gmail.com>
2022-10-25 00:09:12 -0700

    add search function

--- db_func/hotelRoom.ts
+++ db_func/hotelRoom.ts
@@ -1,11 +1,38 @@
-import {HotelRoom} from "../types/types"
-import {db} from "../pages/index"
-import {dbConverter} from "../db_conversion/db_converter"
-import { doc, setDoc, updateDoc } from "firebase/firestore"; 
+import { HotelRoom, SearchFilter } from "../types/types";
+import { db } from "../pages/index";
+import { dbConverter } from "../db_conversion/db_converter";
+import {
+  doc,
+  getDocs,
+  setDoc,
+  updateDoc,
+  collection,
+	query,
+	where,
+} from "firebase/firestore";
 
-export async function updateHotelRoom(hotelRoom : HotelRoom) {
+export async function updateHotelRoom(hotelRoom: HotelRoom) {
+  const docRef = doc(db, "HotelRoom", hotelRoom.id);
 
-  const docRef = doc(db, 'HotelRoom', hotelRoom.id)
-
-  await updateDoc(docRef, await dbConverter.hotelRoomToJson(hotelRoom)) 
+  await updateDoc(docRef, await dbConverter.hotelRoomToJson(hotelRoom));
 }
+
+export async function searchHotel(filter: SearchFilter) : Promise<HotelRoom[]> {
+  const userRef = collection(db, "HotelRoom");
+  let q = query(userRef);
+
+	if(filter.enableLocation) {
+		q = query(q, where("location", "==", filter.location));
+	}
+
+	
+
+	const snapshot = await getDocs(q);
+
+	const rooms = snapshot.docs.map(async (el) => {
+		return await dbConverter.jsonToHotelRoom(el.data(), el.ref);
+	})
+
+	return Promise.all(rooms);
+}
+	
