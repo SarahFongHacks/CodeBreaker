f2c341ea4d34617aa69b160c2001272d43dff0ea
PercyChang18 <79681586+PercyChang18@users.noreply.github.com>
2022-11-08 03:54:49 -0800

    Add Reservation page

---
+++ pages/reservation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReservationPage from '../components/ReservationPage.js';
+
+const reservation = () => {
+  return (
+    <ReservationPage />
+  )
+}
+
+export default reservation
