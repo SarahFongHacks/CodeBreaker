a2e6ca02ce91086a05348a82d54fcb4c72286356
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-11-11 13:05:01 -0800

    update reservation page dataflow

--- components/HotelPage.tsx
+++ components/HotelPage.tsx
@@ -16,12 +16,14 @@ import { motion } from "framer-motion";
 // import "react-dates/lib/css/_datepicker.css";
 import ImageCarousel from "./UI/ImageCarousel";
 import LoginButton from "./UI/LoginButton";
+import ReservationPage from "./ReservationPage";
 
 const Hotel = ({ hotels }) => {
   const [checkin, setCheckin] = useState();
   const [checkout, setCheckout] = useState();
   const [registered, setRegistered] = useState(false);
   const [focusedInput, setFocusedInput] = useState();
+  const [reservation, setReservation] = useState(false);
 
   const router = useRouter();
   const id = router.asPath.slice(7);
@@ -54,77 +56,83 @@ const Hotel = ({ hotels }) => {
   };
 
   return (
-    <motion.div
-      className="w-full h-screen flex flex-col items-center justify-center p-36 relative overflow-hidden"
-      key="main"
-      initial="hidden"
-      animate="visible"
-      variants={container}
-      exit={{ opacity: 0 }}
-    >
-      <div className="absolute left-0 top-0 flex w-full items-center justify-end p-8">
-        <LoginButton color="black" />
-      </div>
-      <div className="overflow-hidden ">
-        <motion.h1
-          variants={item}
-          className="font-bold text-4xl mb-8 self-start"
+    <div>
+      {reservation ? (
+        <ReservationPage hotel={hotel} />
+      ) : (
+        <motion.div
+          className="w-full h-screen flex flex-col items-center justify-center p-36 relative overflow-hidden"
+          key="main"
+          initial="hidden"
+          animate="visible"
+          variants={container}
+          exit={{ opacity: 0 }}
         >
-          {hotel?.hotel}
-        </motion.h1>
-      </div>
-      <motion.div
-        variants={item}
-        className="flex flex-row items-start justify-center space-x-8"
-      >
-        <ImageCarousel images={hotel?.image} />
-        <div className="w-1/2 h-full flex flex-col justify-between items-start">
-          <div className="w-full grid grid-cols-2 gap-4 items-start">
-            <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
-              <div className="flex flex-col ">
-                <p className="font-[600]">Location</p>
-                <p className="text-base">{hotel?.location}</p>
-              </div>
-              <MdLocationPin className="mx-1" />
-            </div>
-            <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
-              <div className="flex flex-col ">
-                <p className="font-[600]">Capacity</p>
-                <p className="text-base">{hotel?.capacity} Guests</p>
-              </div>
-              <MdPeopleAlt className="mx-1" />
-            </div>
-            <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
-              <div className="flex flex-col ">
-                <p className="font-[600]">Bedrooms</p>
-                <p className="text-base">{hotel?.numberOfBeds} Beds</p>
-              </div>
-              <IoIosBed className="mx-1" />
-            </div>
-            <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
-              <div className="flex flex-col ">
-                <p className="font-[600]">Bathrooms</p>
-                <p className="text-base">{hotel?.numberOfBathrooms} Baths</p>
-              </div>
-              <FaToilet className="mx-1" />
-            </div>
-            <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
-              <div className="flex flex-col ">
-                <p className="font-[600]">Room Number</p>
-                <p className="text-base">{hotel?.roomNumber}</p>
-              </div>
-              <BsFillDoorOpenFill className="mx-1" />
-            </div>
-            <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
-              <div className="flex flex-col ">
-                <p className="font-[600]">Rate</p>
-                <p className="text-base">${hotel?.price / 100}/night</p>
-              </div>
-              <FaMoneyBillWave className="mx-1" />
-            </div>
+          <div className="absolute left-0 top-0 flex w-full items-center justify-end p-8">
+            <LoginButton color="black" />
           </div>
-          <div className=" flex flex-row justify-center items-center ">
-            {/* <DateRangePicker
+          <div className="overflow-hidden ">
+            <motion.h1
+              variants={item}
+              className="font-bold text-4xl mb-8 self-start"
+            >
+              {hotel?.hotel}
+            </motion.h1>
+          </div>
+          <motion.div
+            variants={item}
+            className="flex flex-row items-start justify-center space-x-8"
+          >
+            <ImageCarousel images={hotel?.image} />
+            <div className="w-1/2 h-full flex flex-col justify-between items-start">
+              <div className="w-full grid grid-cols-2 gap-4 items-start">
+                <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
+                  <div className="flex flex-col ">
+                    <p className="font-[600]">Location</p>
+                    <p className="text-base">{hotel?.location}</p>
+                  </div>
+                  <MdLocationPin className="mx-1" />
+                </div>
+                <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
+                  <div className="flex flex-col ">
+                    <p className="font-[600]">Capacity</p>
+                    <p className="text-base">{hotel?.capacity} Guests</p>
+                  </div>
+                  <MdPeopleAlt className="mx-1" />
+                </div>
+                <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
+                  <div className="flex flex-col ">
+                    <p className="font-[600]">Bedrooms</p>
+                    <p className="text-base">{hotel?.numberOfBeds} Beds</p>
+                  </div>
+                  <IoIosBed className="mx-1" />
+                </div>
+                <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
+                  <div className="flex flex-col ">
+                    <p className="font-[600]">Bathrooms</p>
+                    <p className="text-base">
+                      {hotel?.numberOfBathrooms} Baths
+                    </p>
+                  </div>
+                  <FaToilet className="mx-1" />
+                </div>
+                <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
+                  <div className="flex flex-col ">
+                    <p className="font-[600]">Room Number</p>
+                    <p className="text-base">{hotel?.roomNumber}</p>
+                  </div>
+                  <BsFillDoorOpenFill className="mx-1" />
+                </div>
+                <div className="flex flex-row w-full items-center justify-between ring-1 ring-black/20 p-6 rounded-md">
+                  <div className="flex flex-col ">
+                    <p className="font-[600]">Rate</p>
+                    <p className="text-base">${hotel?.price / 100}/night</p>
+                  </div>
+                  <FaMoneyBillWave className="mx-1" />
+                </div>
+              </div>
+              <div className=" flex flex-row justify-center items-center ">
+                {/* <DateRangePicker
               daySize={30}
               openDirection={"up"}
               startDate={checkin}
@@ -138,7 +146,7 @@ const Hotel = ({ hotels }) => {
               focusedInput={focusedInput}
               onFocusChange={(focusedInput) => setFocusedInput(focusedInput)}
             /> */}
-            {/* <div className="flex flex-col">
+                {/* <div className="flex flex-col">
               <label htmlFor="startDate" className="font-semibold">
                 Check-in
               </label>
@@ -160,28 +168,29 @@ const Hotel = ({ hotels }) => {
                 onChange={(e) => setCheckout(e.target.value)}
               ></input>
             </div> */}
-          </div>
-          {user ? (
-            <Link href="/reservation">
-            <div
-              className="w-full shadow-lg hover:shadow-xl cursor-pointer hover:scale-[1.01] bg-gradient-to-r from-tertiary to-[#79A1F7] font-bold text-white   py-3 px-5 transition ease-linear duration-200 rounded-md  whitespace-nowrap flex items-center justify-center bg-tertiary"
-              // onClick={() =>
-              //   registrationHandler({ hotel, user, checkin, checkout })
-              // }
-            >
-              Book Now
-            </div>
-          </Link>
-          ) : (
-            <Link href="/login">
-              <div className="w-full shadow-lg hover:shadow-xl hover:scale-[1.01] cursor-pointer font-bold ring-red-500 bg-gradient-to-r to-[#F37979] text-white   py-3 px-5 ring-1 transition ease-linear duration-200 rounded-md  whitespace-nowrap flex items-center justify-center from-red-500">
-                Login to book
               </div>
-            </Link>
-          )}
-        </div>
-      </motion.div>
-    </motion.div>
+              {user ? (
+                <div
+                  className="w-full shadow-lg hover:shadow-xl cursor-pointer hover:scale-[1.01] bg-gradient-to-r from-tertiary to-[#79A1F7] font-bold text-white   py-3 px-5 transition ease-linear duration-200 rounded-md  whitespace-nowrap flex items-center justify-center bg-tertiary"
+                  // onClick={() =>
+                  //   registrationHandler({ hotel, user, checkin, checkout })
+                  // }
+                  onClick={() => setReservation(true)}
+                >
+                  Book Now
+                </div>
+              ) : (
+                <Link href="/login">
+                  <div className="w-full shadow-lg hover:shadow-xl hover:scale-[1.01] cursor-pointer font-bold ring-red-500 bg-gradient-to-r to-[#F37979] text-white   py-3 px-5 ring-1 transition ease-linear duration-200 rounded-md  whitespace-nowrap flex items-center justify-center from-red-500">
+                    Login to book
+                  </div>
+                </Link>
+              )}
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </div>
   );
 };
 
--- components/ReservationPage.js
+++ components/ReservationPage.js
@@ -1,76 +1,79 @@
-import { useState } from 'react';
+import { useState } from "react";
+import ImageCarousel from "./UI/ImageCarousel";
 
-const ReservationPage = (props) => {
-    const [checkin, setCheckin] = useState();
-    const [checkout, setCheckout] = useState();
-    const [numGuest, setNumGuest] = useState();
+const ReservationPage = ({ hotel }) => {
+  const [checkin, setCheckin] = useState();
+  const [checkout, setCheckout] = useState();
+  const [numGuest, setNumGuest] = useState();
 
-    const reservationHandler = ({ hotel, user, checkin, checkout }) => {
-        createReservation(hotel, user, new Date(checkin), new Date(checkout)).then(
-          (res) => {
-            // res.error === false && setRegistered(true);
-            res.error === false && alert("Hotel was successfully booked!");
-          }
-        );
-      };
-
-    return (
-        <div className='reservation-page'>
-            <div className="text-primary px-8 rounded-lg text-center my-8">
-            <h1 className="text-3xl font-bold">Hotel name</h1>
-            </div>
-
-            <div className='flex justify-around '>
-                <div className='border-2 border-dashed rounded-lg w-2/5'>
-                    <h1>Photo and location</h1>
-                </div>
-                <div className='border-2 border-dashed rounded-lg w-2/5'>
-                    <div className="text-center my-4">
-                        <h3 className="text-1xl">Rate per night</h3>
-                    </div>
-                    <form className='flex flex-col justify-around items-center'>
-                        <div className='check-in'>
-                            <label>Check in: </label>
-                            <input 
-                            type='date' 
-                            name='checkin-date'
-                            className='w-full rounded-md px-3 mb-4 py-2 placeholder-black/50 focus:outline-none ring-1 ring-black focus:ring-tertiary text-black'
-                            onChange={(e) => setCheckin(e.target.value)}
-                            />
-                        </div>
-                        <div className='check-out'>
-                            <label>Check out: </label>
-                            <input
-                            type='date'
-                            name='checkout-date'
-                            className='w-full rounded-md px-3 mb-4 py-2 placeholder-black/50 focus:outline-none ring-1 ring-black focus:ring-tertiary text-black'
-                            onChange={(e) => setCheckout(e.target.value)}
-                            />
-                        </div>
-                        <div className='num-of-guest'>
-                            <label>Number of Guests: </label>
-                            <input
-                            type='text'
-                            name='guest-num'
-                            className='w-full rounded-md px-3 mb-4 py-2 placeholder-black/50 focus:outline-none ring-1 ring-black focus:ring-tertiary text-black'
-                            onChange={(e) => setNumGuest(e.target.value)}/>
-                        </div>
-                        <div className='mb-4'>
-                            <h4>Room Fee: </h4>
-                        </div>
-                        <div className=" mb-4 shadow-md w-1/3 cursor-pointer  hover:bg-white  hover:ring-1 hover:ring-tertiary hover:text-tertiary transition ease-linear duration-200 rounded-md bg-tertiary text-white p-2 flex items-center justify-center"
-                        onClick={() =>
-                            // registrationHandler({ hotel, user, checkin, checkout })
-                            alert("Hotel was successfully booked!")
-                        }>
-                            Reserve
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        
+  const reservationHandler = ({ hotel, user, checkin, checkout }) => {
+    createReservation(hotel, user, new Date(checkin), new Date(checkout)).then(
+      (res) => {
+        // res.error === false && setRegistered(true);
+        res.error === false && alert("Hotel was successfully booked!");
+      }
     );
-}
+  };
+
+  return (
+    <div className="reservation-page">
+      <div className="text-primary px-8 rounded-lg text-center my-8">
+        <h1 className="text-3xl font-bold">{hotel?.hotel}</h1>
+      </div>
+
+      <div className="flex justify-around ">
+        <div className="border-2 border-dashed rounded-lg w-2/5">
+          <ImageCarousel images={hotel?.image} />
+        </div>
+        <div className="border-2 border-dashed rounded-lg w-2/5">
+          <div className="text-center my-4">
+            <h3 className="text-1xl">${hotel?.price / 100}/night</h3>
+          </div>
+          <form className="flex flex-col justify-around items-center">
+            <div className="check-in">
+              <label>Check in: </label>
+              <input
+                type="date"
+                name="checkin-date"
+                className="w-full rounded-md px-3 mb-4 py-2 placeholder-black/50 focus:outline-none ring-1 ring-black focus:ring-tertiary text-black"
+                onChange={(e) => setCheckin(e.target.value)}
+              />
+            </div>
+            <div className="check-out">
+              <label>Check out: </label>
+              <input
+                type="date"
+                name="checkout-date"
+                className="w-full rounded-md px-3 mb-4 py-2 placeholder-black/50 focus:outline-none ring-1 ring-black focus:ring-tertiary text-black"
+                onChange={(e) => setCheckout(e.target.value)}
+              />
+            </div>
+            <div className="num-of-guest">
+              <label>Number of Guests: </label>
+              <input
+                type="text"
+                name="guest-num"
+                className="w-full rounded-md px-3 mb-4 py-2 placeholder-black/50 focus:outline-none ring-1 ring-black focus:ring-tertiary text-black"
+                onChange={(e) => setNumGuest(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <h4>Room Fee: </h4>
+            </div>
+            <div
+              className=" mb-4 shadow-md w-1/3 cursor-pointer  hover:bg-white  hover:ring-1 hover:ring-tertiary hover:text-tertiary transition ease-linear duration-200 rounded-md bg-tertiary text-white p-2 flex items-center justify-center"
+              onClick={() =>
+                // registrationHandler({ hotel, user, checkin, checkout })
+                alert("Hotel was successfully booked!")
+              }
+            >
+              Reserve
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ReservationPage;
