b706d422a4787e680615f049f90a485ba6c89a70
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-11-14 17:01:25 -0800

    add min dates

--- components/ReservationPage.js
+++ components/ReservationPage.js
@@ -25,7 +25,7 @@ const ReservationPage = ({ hotel }) => {
   };
 
   const totalHandler = () => {
-    if (startDate && endDate && endDate > startDate) {
+    if (startDate && endDate && endDate >= startDate) {
       setError(false);
       const total = (endDate - startDate) / 8640000000;
       if (total >= 0) {
@@ -73,6 +73,7 @@ const ReservationPage = ({ hotel }) => {
                 selected={startDate}
                 onChange={(date) => setStartDate(date)}
                 excludeDates={excludedDates}
+                minDate={new Date()}
               />
               {/* <input
                 type="date"
@@ -88,6 +89,7 @@ const ReservationPage = ({ hotel }) => {
                 className="w-full rounded-md px-3 mb-4 py-2 placeholder-black/50 focus:outline-none ring-1 ring-black focus:ring-tertiary text-black"
                 selected={endDate}
                 onChange={(date) => setEndDate(date)}
+                minDate={new Date()}
               />
               {/* <input
                 type="date"
