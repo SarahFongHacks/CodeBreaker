018e23a532c8189f2d5cca5fcbe5c598a4d16e9b
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-11-17 13:55:54 -0800

    update reservation handler

--- components/ReservationPage.js
+++ components/ReservationPage.js
@@ -1,26 +1,25 @@
 import { useContext, useEffect, useState } from "react";
 import ImageCarousel from "./UI/ImageCarousel";
-import { createProduct } from "../stripe/stripe_product";
 import { createReservation } from "../db_func/reservations";
 import { LoginContext } from "../context";
-import { loadStripe } from "@stripe/stripe-js";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { HotelRoom } from "../types/types";
+import { createProduct } from "../stripe/stripe_product";
 import { useRouter } from "next/router";
 
 const ReservationPage = ({ hotel }) => {
+  var today = new Date();
+
   const [total, setTotal] = useState(0);
   const [error, setError] = useState(false);
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(today);
+  var minCheckout = new Date(startDate);
+  minCheckout.setDate(minCheckout.getDate() + 1);
+  const [endDate, setEndDate] = useState(minCheckout);
 
   const { user } = useContext(LoginContext);
-
-  const stripePromise = loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-  );
 
   const router = useRouter();
 
@@ -40,7 +39,7 @@ const ReservationPage = ({ hotel }) => {
   };
 
   const totalHandler = () => {
-    if (startDate && endDate && endDate >= startDate) {
+    if (startDate && endDate && endDate > startDate) {
       setError(false);
       const total = (endDate - startDate) / 8640000000;
       if (total >= 0) {
@@ -53,29 +52,27 @@ const ReservationPage = ({ hotel }) => {
   };
 
   useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-    if (query.get("success")) {
-      console.log("Order placed! You will receive an email confirmation.");
-    }
-
-    if (query.get("canceled")) {
-      console.log(
-        "Order canceled -- continue to shop around and checkout when you’re ready."
-      );
-    }
-  }, []);
-
-  useEffect(() => {
     totalHandler();
   }, [startDate, endDate]);
 
   const excludedDates = [];
+  for (let i = 0; i < hotel.reservations.length; i++) {
+    excludedDates.push({
+      start: new Date(hotel.reservations[i].startDate),
+      end: new Date(hotel.reservations[i].endDate),
+    });
+    //console.log(new Date(hotel.reservations[i].startDate) + ", " + new Date(hotel.reservations[i].endDate));
+  }
+
+  const disableDateRange = excludedDates.map((range) => ({
+    start: range.start,
+    end: range.end,
+  }));
 
   return (
     <div className="bg-gradient-to-b from-white to-tertiary/10 w-full h-screen flex items-center justify-center flex-col p-16">
       <div className="text-primary px-8 rounded-lg text-center my-8">
-        <h1 className="text-4xl font-bold">{hotel?.hotel}</h1>
+        <h1 className="text-3xl font-bold">{hotel?.hotel}</h1>
       </div>
 
       <div className="flex space-x-8 w-full items-center justify-center ">
@@ -101,7 +98,8 @@ const ReservationPage = ({ hotel }) => {
                 className="w-full rounded-md px-3 mb-4 py-2 placeholder-black/50 focus:outline-none ring-1 ring-black focus:ring-tertiary text-black"
                 selected={startDate}
                 onChange={(date) => setStartDate(date)}
-                excludeDates={excludedDates}
+                //excludeDates={excludedDates}
+                excludeDateIntervals={disableDateRange}
                 minDate={new Date()}
               />
               {/* <input
@@ -117,8 +115,9 @@ const ReservationPage = ({ hotel }) => {
               <DatePicker
                 className="w-full rounded-md px-3 mb-4 py-2 placeholder-black/50 focus:outline-none ring-1 ring-black focus:ring-tertiary text-black"
                 selected={endDate}
+                excludeDateIntervals={disableDateRange}
                 onChange={(date) => setEndDate(date)}
-                minDate={new Date()}
+                minDate={minCheckout}
               />
               {/* <input
                 type="date"
