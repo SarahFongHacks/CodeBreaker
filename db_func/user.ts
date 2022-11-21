a6a298a73d7678e4f3c992922785e8bd8e355db7
Jugad Khajuria <curaww@gmail.com>
2022-11-20 22:22:24 -0800

    add get user function

--- db_func/user.ts
+++ db_func/user.ts
@@ -1,11 +1,16 @@
 import {User} from "../types/types"
 import {db} from "../pages/index"
 import {dbConverter} from "../db_conversion/db_converter"
-import { doc, setDoc, updateDoc } from "firebase/firestore"; 
+import { doc, setDoc, updateDoc, getDoc } from "firebase/firestore"; 
 
 export async function updateUser(user : User) {
 
   const docRef = doc(db, 'User', user.id)
 
   await updateDoc(docRef, await dbConverter.userToJson(user)) 
+}
+
+export async function getUser(userId : string) : Promise<User> {
+
+  return await dbConverter.jsonToUser(await getDoc(doc(db, 'User', userId)), doc(db, 'User', userId))
 }
