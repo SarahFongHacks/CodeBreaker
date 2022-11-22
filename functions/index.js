4c0e26169177b5f84b1861ac7b225b9881eea20a
JugadK <curaww@gmail.com>
2022-11-21 23:17:45 -0800

    update cloud functions

--- functions/index.js
+++ functions/index.js
@@ -7,20 +7,23 @@
 // });
 //
 
-const { warn } = require("console");
+const {warn} = require("console");
 const functions = require("firebase-functions");
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 const fetch = require("node-fetch");
 const stripe = require("stripe")(
-  "sk_test_51LxMk8GjvQ8kI9Vg4nK5grQV5FEuWWVolO1kuVg8NEjC8OsMWmUoBP0LpPSEHwGrSXq0wLOqK5Eex3ZzqeKMGWf700PAvEAQ3J"
-);
+    "sk_test_51LxMk8GjvQ8kI9Vg4nK5grQV5FEuWWVolO1kuVg8NEjC8OsMWmUoBP0LpPSEHwGrSXq0wLOqK5Eex3ZzqeKMGWf700PAvEAQ3J");
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
 
 exports.helloWorld = functions.https.onCall(async (request, response) => {
-
   const product = await stripe.products.create({
-    name: request.productName,
-    images: request.images,
+    name : request.productName,
+    images : request.images,
   });
 
   const price = await stripe.prices.create({
@@ -37,8 +40,98 @@ exports.helloWorld = functions.https.onCall(async (request, response) => {
   return ret;
 });
 
-exports.checkoutComplete = functions.https.onRequest((request, response) => {
-  
-  console.log(request.body.data.object.metadata)
+exports.refund = functions.https.onCall(async (request, response) => {
+  const reservation = JSON.parse(request.body.reservation)
 
+  try {
+    const refund = await stripe.refunds.create(
+        {payment_intent : reservation.paymentIntent});
+
+  } catch (error) {
+    return false;
+  }
+
+  return true;
 });
+
+exports.checkoutComplete =
+    functions.https.onRequest(async (request, response) => {
+      // console.log(request.body.data.object.metadata)
+
+      //	console.log(request.body)
+
+      const userId = request.body.data.object.metadata.user
+      const hotelId = request.body.data.object.metadata.hotel
+      const startDate = request.body.data.object.metadata.start
+      const endDate = request.body.data.object.metadata.end
+      const type = request.body.data.object.metadata.type
+
+      if (type == "cancel") {
+
+        const reservation =
+            JSON.parse(request.body.data.object.metadata.reservation)
+
+       // console.log(reservation)
+
+        const hotelDocRef =
+            db.collection('HotelRoom').doc(reservation.hotelRoomId)
+        const userDocRef = db.collection('User').doc(reservation.userId)
+        const user = (await userDocRef.get()).data()
+
+        const hotelRoom = (await hotelDocRef.get()).data()
+
+        console.log(user)
+      	console.log(hotelRoom)
+        //
+
+        for (let i = 0; i < hotelRoom.reservations.length; i++) {
+          if (hotelRoom.reservations[i].id == reservation.id)
+            hotelRoom.reservations.splice(i, 1)
+        }
+
+        for (let i = 0; i < user.currentBooking.length; i++) {
+
+					const id = (await user.currentBooking[i].get()).data().id
+					
+          if (id == reservation.id)
+            user.currentBooking.splice(i, 1)
+        }
+
+        userDocRef.set(user)
+        hotelDocRef.set(hotelRoom)
+        const resCollectionRef = db.collection("Reservation");
+        const resDocRef = resCollectionRef.doc(reservation.id);
+
+        resDocRef.delete();
+      }
+      else {
+
+        const userDocRef = db.collection('User').doc(userId)
+        const hotelDocRef = db.collection('HotelRoom').doc(hotelId)
+
+        const user = (await userDocRef.get()).data()
+        const hotel = (await hotelDocRef.get()).data()
+
+        const resCollectionRef = db.collection("Reservation");
+        const resDocRef = resCollectionRef.doc();
+        const id = resDocRef.id;
+
+        const reservation = {
+          id : id,
+          endDate : Date.parse(endDate),
+          hotelRoomId : hotelId,
+          startDate : Date.parse(startDate),
+          userId : userId,
+        }
+
+        resDocRef.set(reservation)
+        user.currentBooking.push(resDocRef)
+        hotel.reservations.push(resDocRef)
+
+        userDocRef.set(user)
+        hotelDocRef.set(hotel)
+      }
+      console.log("Hellofdfdsafdsaf");
+
+      return response;
+    });
