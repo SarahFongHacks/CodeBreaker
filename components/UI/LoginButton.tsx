8fc51e4cf71f3d8e138e613803eafb382565fe72
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-11-27 14:11:43 -0800

    update login button

--- components/UI/LoginButton.tsx
+++ components/UI/LoginButton.tsx
@@ -27,13 +27,15 @@ const LoginButton = ({ color }) => {
     setUser(undefined);
   };
 
+  console.log((3.99999999).toFixed(2));
+
   return (
     <div className="select-none ">
       {user ? (
         <div className="h-12 flex space-x-4 ">
           <div className=" flex flex-row items-center justify-center space-x-2 rounded-lg bg-white/70 backdrop-blur-xl ring-1 truncate overflow-hidden py-3 px-5 ring-black/30 transition duration-200 ease-linear ">
             <IoTicketSharp />
-            <p>{user.rewardPoints.toFixed(0)}</p>
+            <p>{Math.floor(user.rewardPoints)}</p>
           </div>
           <DropdownMenu.Root>
             <DropdownMenu.Trigger className="select-none focus:outline-none">
